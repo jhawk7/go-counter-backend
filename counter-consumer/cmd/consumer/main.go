6fb77c6f95c8f911c31c9e5f01ff8c1b78fd47d7
jhawk7 <jhawk@ser>
2024-10-09 15:47:56 -0400

    building kafka consumer

--- counter-consumer/cmd/consumer/main.go
+++ counter-consumer/cmd/consumer/main.go
@@ -36,7 +36,7 @@ func InitDB(config *common.Config) {
 }
 
 func InitConsumer(config *common.Config) {
-	kconn, kErr := kafka.DialLeader(context.Background(), "tcp", config.KafkaUrl, config.KafkaTopic, 0)
+	kconn, kErr := kafka.DialLeader(context.Background(), "tcp", config.KafkaHost, config.KafkaTopic, 0)
 	if kErr != nil {
 		panic(fmt.Errorf("failed to establish connection to kafka; %v", kErr))
 	}
@@ -58,54 +58,25 @@ func main() {
 		})
 	})
 
-	// r.GET("/count", GetCount)
-	// r.PUT("/count", ResetCount)
-	// r.PATCH("/count", UpdateCount)
 	go ReadStream()
 	r.Run(":8888")
 }
 
 func ReadStream() {
+	ctx := context.Background()
+	ch := make(chan event.Msg)
+	go kConsumer.ReadMsg(ch)
 
+	for m := range ch {
+		switch m.Event {
+		case "increment":
+			err := db.IncrementCount(ctx)
+			common.LogError(err, false)
+		case "reset":
+			err := db.SetValue(ctx, 0)
+			common.LogError(err, false)
+		default:
+			common.LogError(fmt.Errorf("unexpected event message"), false)
+		}
+	}
 }
-
-// func GetCount(c *gin.Context) {
-// 	val, err := db.GetValue(c)
-// 	if err != nil {
-// 		c.Error(err)
-// 		return
-// 	}
-
-// 	c.JSON(http.StatusOK, gin.H{
-// 		"count": val,
-// 	})
-// }
-
-// func ResetCount(c *gin.Context) {
-// 	type Params struct {
-// 		Counter int `json:"counter"`
-// 	}
-
-// 	var params Params
-// 	if bindErr := c.ShouldBindJSON(&params); bindErr != nil {
-// 		err := fmt.Errorf("missing counter param in request")
-// 		c.Error(err)
-// 		c.AbortWithStatusJSON(http.StatusBadRequest, err)
-// 	}
-
-// 	if err := db.SetValue(c, params.Counter); err != nil {
-// 		c.Error(err)
-// 		return
-// 	}
-
-// 	c.Status(http.StatusNoContent)
-// }
-
-// func UpdateCount(c *gin.Context) {
-// 	if err := db.IncrementCount(c); err != nil {
-// 		c.Error(err)
-// 		return
-// 	}
-
-// 	c.Status(http.StatusNoContent)
-// }
